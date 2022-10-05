106635a283c46cc7d2a6c5178f0abd41b38589a6
Agustin Pieres <pieresagustin@gmail.com>
2022-10-05 13:45:34 -0300

    ProductosService: - Crear metodo Create, FindAll y FindOne

--- src/productos/productos.service.ts
+++ src/productos/productos.service.ts
@@ -1,19 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateProductoDto } from './dto/create-producto.dto';
 import { UpdateProductoDto } from './dto/update-producto.dto';
+import { Producto } from './entities/producto.entity';
 
 @Injectable()
 export class ProductosService {
+  constructor(
+    @InjectRepository(Producto)
+    private productoRepository: Repository<Producto>,
+  ) {}
+
   create(createProductoDto: CreateProductoDto) {
-    return 'This action adds a new producto';
+    const producto: Producto =
+      this.productoRepository.create(createProductoDto);
+    return this.productoRepository.save(producto);
   }
 
   findAll() {
-    return `This action returns all productos`;
+    return this.productoRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} producto`;
+    return this.productoRepository.findOneBy({ id });
   }
 
   update(id: number, updateProductoDto: UpdateProductoDto) {
