0b51543c2a4e0a12b72a92fe67a952c57d9047ae
Agustin Pieres <pieresagustin@gmail.com>
2022-10-05 14:08:42 -0300

    Productos Crear DTO y Entity

--- src/productos/dto/create-producto.dto.ts
+++ src/productos/dto/create-producto.dto.ts
@@ -1,1 +1,7 @@
-export class CreateProductoDto {}
+export class CreateProductoDto {
+  id: number;
+  tipo: string;
+  talle: string;
+  color: string;
+  precio: number;
+}
--- src/productos/entities/producto.entity.ts
+++ src/productos/entities/producto.entity.ts
@@ -1,1 +1,15 @@
-export class Producto {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Producto {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  tipo: string;
+  @Column()
+  talle: string;
+  @Column()
+  color: string;
+  @Column()
+  precio: number;
+}
