23955f1e4356f55232bda3ebf47996c0ac0193e5
Agustin Pieres <pieresagustin@gmail.com>
2022-10-09 11:19:21 -0300

    Deploy to Render

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,19 @@ import { ProductosModule } from './productos/productos.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Producto } from './productos/entities/producto.entity';
 import { BrandsModule } from './brands/brands.module';
+import { Brand } from './brands/entities/brand.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
+      url: process.env.DATABASE_URL,
       host: 'localhost',
       port: 5432,
       username: 'agustin',
       password: '12345',
       database: 'Tienda',
-      entities: [Producto],
+      entities: [Producto, Brand],
       synchronize: true,
     }),
     ProductosModule,
