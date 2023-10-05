bceaf83374772c2ab7ee81ac6d2cb12752c3bb97
Agustin Pieres <pieresagustin@gmail.com>
2023-10-05 18:07:59 -0300

    Add tests to Brands controller specs

--- src/brands/brands.controller.spec.ts
+++ src/brands/brands.controller.spec.ts
@@ -5,16 +5,40 @@ import { BrandsService } from './brands.service';
 describe('BrandsController', () => {
   let controller: BrandsController;
 
+  const brandsMock = {
+    brands: [
+      {
+        name: 'Nike',
+        brand_id: 1,
+      },
+      {
+        brand_id: 2,
+        marca: 'Adidas',
+      },
+    ],
+    findAll() {
+      return this.brands;
+    },
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BrandsController],
       providers: [BrandsService],
-    }).compile();
+    })
+      .overrideProvider(BrandsService)
+      .useValue(brandsMock)
+      .compile();
 
     controller = module.get<BrandsController>(BrandsController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('Should show me an array of brands', async () => {
+    const result = await controller.findAll();
+    expect(result).toEqual(brandsMock.brands);
   });
 });
