0ed9b81e4f2e9f13c4b8de07b01467fb6b65d37c
Agustin Pieres <pieresagustin@gmail.com>
2023-10-05 18:12:14 -0300

    Create products controller spec

---
+++ src/products/product.controller.spec.ts
@@ -0,0 +1,62 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ProductsController } from './product.controller';
+
+describe('ProductsController', () => {
+  let controller: ProductsController;
+
+  const productsMock = {
+    products: [
+      {
+        id: 1,
+        name: 'Camiseta Nike',
+        type: 'Camiseta',
+        waist: 'M',
+        color: 'White',
+        stock: 30,
+        price: 1000,
+        image: '',
+        quantity: 30,
+        clearance: false,
+        brand: {
+          brand_id: 1,
+          marca: 'Adidas',
+        },
+      },
+      {
+        id: 2,
+        name: 'Camiseta Addidas',
+        type: 'Camiseta',
+        waist: 'XL',
+        color: 'Blue and Yellow',
+        stock: 20,
+        price: 1500,
+        image: '',
+        quantity: 20,
+        clearance: false,
+        brand: {
+          brand_id: 1,
+          marca: 'Adidas',
+        },
+      },
+    ],
+    findOne() {
+      return this.products;
+    },
+  };
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({}).compile();
+
+    controller = module.get<ProductsController>(ProductsController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  // it('Should find a product by an ID', async () => {
+  //   const result = await controller.findOne('1');
+  //   expect(result).toContain(productsMock.products{
+  //     id:1
+  //   }),
+  // });
+});
